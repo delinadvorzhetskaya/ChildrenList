4ed5f0d85608bba764990b7b29936697948cffa4
Delina Dvorzhetskaya <delina.dvorzhetskaya@gmail.com>
2022-02-24 20:41:14 +0300

    Initial Commit

---
+++ ChildrenList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChildrenList
+//
+//  Created by Дэлина Дворжецкая on 24.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
